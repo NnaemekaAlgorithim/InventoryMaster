c135fe9e8ad46f2b4e1efa59f4719dfe1932ce34
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-06-24 00:55:30 +0100

    This class is used to create a specific user record in database

--- models/users_record_update.py
+++ models/users_record_update.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Defines the User class."""
-from hashlib import md5
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -21,16 +20,7 @@ class User(BaseModel, Base):
         sales (sqlalchemy relationship): The User-Sales relationship.
     """
     __tablename__ = "users"
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    inventory = relationship("Inventory", backref="user", cascade="delete")
-    sales = relationship("Sales", backref="user", cascade="delete")
-
-    def __setattr__(self, name, value):
-        """Encodes passwords using MD5 before setting an attribute."""
-        if name == "password":
-            value = value.encode("utf-8")
-            value = md5(value).hexdigest()
-        object.__setattr__(self, name, value)
